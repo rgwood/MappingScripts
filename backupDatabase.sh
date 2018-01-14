4f41275f8419d6e0500f516519e333b3da1217e2
Reilly <reilly.wood@icloud.com>
2018-01-14 10:26:53 -0800

    copy backup to cloud

--- backupDatabase.sh
+++ backupDatabase.sh
@@ -1,2 +1,10 @@
 # https://www.commandprompt.com/blog/a_better_backup_with_postgresql_using_pg_dump/
+
+echo "Backing up..."
 pg_dump -h localhost -U docker --format=c --file=gisBackup.sqlc gis
+
+echo "Backed up. Copying to ~Documents/DatabaseBackups"
+cp gisBackup.sqlc ~/Documents/DatabaseBackups/gisBackup-$(date +%Y%m%d).sqlc
+
+
+echo "Done!"
