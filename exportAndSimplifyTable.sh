d167205218d9706874f7772124293bdb3a821334
Reilly <reilly.wood@icloud.com>
2018-01-22 22:05:55 -0800

    new view

--- exportAndSimplifyTable.sh
+++ exportAndSimplifyTable.sh
@@ -1,6 +1,6 @@
 echo "Exporting from DB to GeoJSON"
 ogr2ogr -f GeoJSON parcels.geojson  "PG:host=${1:-localhost} dbname=${2:-gis} user=${3:-docker}" \
--sql "select * FROM public.van_parcels_for_export" -t_srs "EPSG:4326"; 
+-sql "select * FROM public.filtered_parcels_for_export" -t_srs "EPSG:4326"; 
 
 echo "Running Tippecanoe"
 tippecanoe -o parcels.mbtiles parcels.geojson --coalesce-smallest-as-needed -f -z 15 -Z 5 -l default;
