6e09a923eff60e53778795d70643f73a25ae60bf
Reilly <reilly.wood@icloud.com>
2018-01-26 22:00:23 -0800

    bigger tile size to improve quality at lower zoom

--- exportAndSimplifyTable.sh
+++ exportAndSimplifyTable.sh
@@ -3,9 +3,10 @@ ogr2ogr -f GeoJSON parcels.geojson  "PG:host=${1:-localhost} dbname=${2:-gis} us
 -sql "select * FROM public.filtered_parcels_for_export" -t_srs "EPSG:4326"; 
 
 echo "Running Tippecanoe"
-tippecanoe -o parcels.mbtiles parcels.geojson --coalesce-smallest-as-needed -f -z 15 -Z 5 -l default;
+tippecanoe -o parcels.mbtiles parcels.geojson  -f -z 15 -Z 8 -l default -Bg --coalesce-smallest-as-needed --maximum-tile-bytes=1000000;
+#tippecanoe -o parcels.mbtiles parcels.geojson --no-tiny-polygon-reduction --no-feature-limit --no-tile-size-limit -f -z 15 -Z 5 -l default;
 #tippecanoe -o parcels.mbtiles --drop-densest-as-needed parcels.geojson -f -z 15 -Z 5 -l default;
-
+#--coalesce-smallest-as-needed
 rm parcels.geojson
 
 echo "Moving exported file to tile server folder"
